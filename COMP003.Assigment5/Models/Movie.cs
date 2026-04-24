e288ebbb25dfbbb8348c8a388ba0513fb78a2ab1
evanbern59 <evansantos1202@gmail.com>
2026-04-23 21:49:05 -0700

    added the model for movie.cs

---
+++ COMP003.Assigment5/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003.Assigment5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public decimal Rating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
